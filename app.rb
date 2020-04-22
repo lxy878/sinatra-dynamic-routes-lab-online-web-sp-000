de6850d2747eee7e1f1f8fbe6c540495e6061b87
Xiaoyi Liu <lxy878@gmail.com>
2020-04-22 13:19:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-    params[:name]
-    puts params.inspect
+    @reverse = params[:name].reverse
+    "#{@reverse}"
   end
 
   get '/square/:nubmer' do
